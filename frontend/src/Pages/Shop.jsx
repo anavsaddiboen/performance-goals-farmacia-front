2e7eca4cc336741a59860e9110e60b319afa7946
anavsaddiboen <anavsaddi@gmail.com>
2024-04-22 16:09:00 -0300

    Performance Goals

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 export const Shop = () => {
   return (
     <div>
-      <Hero />
-      <Popular />
+      <Hero></Hero>
+      <Popular></Popular>
+      <NewCollections></NewCollections>
+      <NewsLetter></NewsLetter>
     </div>
   )
 }
+
+
 
 export default Shop
